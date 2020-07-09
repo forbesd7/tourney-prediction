6e2d5994499119a1cc506b3869060e76e9faeff0
Derek <derek.forbes77@gmail.com>
2020-07-09 21:50:28 +0900

    changed it to be adding to public tournament doc

--- src/components/CreateTournament/db-funcs.ts
+++ src/components/CreateTournament/db-funcs.ts
@@ -3,16 +3,22 @@ import { firestore } from "../../firebase";
 import { User } from "firebase";
 import { TournamentInfo } from "../../providers/CreatedTournamentProvider";
 
-const userCollection = firestore.collection("users");
 export const addTournament = async (
   user: User,
   tournamentInfo: TournamentInfo
 ) => {
   const userId = user.uid;
-  await userCollection.doc(userId).update({
-    created_tournament: firebase.firestore.FieldValue.arrayUnion(
-      tournamentInfo
-    ),
-  });
+
+  const userDoc = firestore.collection("tournaments").doc(userId);
+  const userDocData = await userDoc.get();
+  if (userDocData.exists) {
+    userDoc.update({
+      createdTournaments: firebase.firestore.FieldValue.arrayUnion(
+        tournamentInfo
+      ),
+    });
+  } else {
+    userDoc.set({ createdTournaments: tournamentInfo });
+  }
   console.log("entered that shit");
 };
