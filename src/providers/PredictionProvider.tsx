27db2df84e6451e6707e5c696e68c253d8a2e0da
Derek <derek.forbes77@gmail.com>
2020-08-08 22:38:32 +0900

    starting context add

---
+++ src/providers/PredictionProvider.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState, useEffect } from "react";
+
+interface Matchups {
+  A: string;
+  B: string;
+}
+
+export interface MatchupInfo {
+  [key: string]: Matchups;
+}
+
+export interface TournamentInfo {
+  numOfPlayers: number;
+  matchupInfo: MatchupInfo;
+  name: string;
+}
+
+interface CreatedTournamentContext {
+  tournamentInfo: TournamentInfo;
+  updateInfo: React.Dispatch<React.SetStateAction<TournamentInfo>>;
+}
+
+const defaultCreatedTournamentContext: CreatedTournamentContext = {
+  tournamentInfo: {
+    numOfPlayers: 8,
+    matchupInfo: {},
+    name: "",
+  },
+  updateInfo: (): void => {},
+};
+
+export const defaultTournamentInfo = {
+  numOfPlayers: 8,
+  name: "",
+  matchupInfo: {},
+};
+
+export const createdTournamentContext = createContext<CreatedTournamentContext>(
+  defaultCreatedTournamentContext
+);
+
+export const CreatedTournamentProvider: React.FC = ({ children }) => {
+  const [tournamentInfo, updateInfo] = useState<TournamentInfo>(
+    defaultTournamentInfo
+  );
+  return (
+    <createdTournamentContext.Provider
+      value={{
+        tournamentInfo,
+        updateInfo,
+      }}
+    >
+      {children}
+    </createdTournamentContext.Provider>
+  );
+};
